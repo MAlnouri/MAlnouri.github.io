a9355fe1b497cd381ce4209969bdbb36d9c95ce5
AlnouriLaptop <alnourimahmmoud@gmail.com>
2019-10-28 12:20:01 -0700

    js

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,22 @@
+const myString = 'Learning Javascript';
+
+const myFirstVariable = 3;
+const myBoolean = true;
+let myUndefined;
+
+var oldStyle = 'The old way to do it.';
+let newStyle = 'This new way, dynamic variable, can be changed.';
+const constantVariable = 'Constants cannot be changed. Except...';
+
+const quotes = 'Escape a \' quotation mark.';
+
+myUndefined = 25;
+
+console.log(myString);
+console.log('Type:', typeof myString);
+console.log(myFirstVariable);
+console.log('Type:',typeof myFirstVariable);
+console.log(myBoolean);
+console.log('Type:',typeof myBoolean);
+console.log(myUndefined);
+console.log('Type:',typeof myUndefined);
