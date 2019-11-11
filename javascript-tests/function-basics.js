ada93d48345bd8f360e10f2c93b8bc5ad42272af
MAlnouri <alnourimahmmoud@gmail.com>
2019-11-10 21:48:22 -0800

    js functions complete

---
+++ javascript-tests/function-basics.js
@@ -0,0 +1,53 @@
+function simpleFunction() {
+    console.log("functions should consist of statements designed to perform a single task.");
+}
+
+function functionParameters(printMe) {
+    console.log(printMe);
+}
+
+function functionReturn() {
+    return "Many functions return values.";
+}
+
+window.onload = () => {
+    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+    const functionParametersDisplay = document.getElementById('functionParametersDisplay');
+    const functionReturnDisplay = document.getElementById('functionReturnDisplay');
+    const functionCallsAction = document.getElementById('functionCallsAction');
+
+    const addToListAction = document.getElementById('addToListAction');
+    
+    function simpleFunction() {
+        console.log('Button adds to simple paragraph element.');
+        simpleFunctionDisplay.textContent = 'functions should consist of statements designed to perform a single task.';
+    }
+
+    function parameterFunction() {
+        console.log('Button adds to paramenter paragraph element.');
+        functionParametersDisplay.textContent = 'Prints the parameter of the function.';
+    }
+
+    function returnFunction() {
+        console.log('Button adds to return paragraph element');
+        functionReturnDisplay.textContent = 'Many functions return values.';
+    }
+
+    function callAllFunctions() {
+        simpleFunction();
+        parameterFunction();
+        returnFunction();
+    }
+
+    functionCallsAction.onclick = callAllFunctions;
+
+    addToListAction.onclick = () => {
+        const inputValue = document.getElementById('nameDisplay').value;
+        elfCode.appendToList(listDisplay, inputValue);
+        console.log('Added user input to list.');
+    }
+}
+
+simpleFunction();
+functionParameters("Many functions take parameters.");
+console.log(functionReturn());
