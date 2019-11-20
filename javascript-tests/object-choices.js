4780cfda7cee42197bc4f6262f98e413fb37630a
AlnouriLaptop <alnourimahmmoud@gmail.com>
2019-11-20 12:18:02 -0800

    objects

--- javascript-tests/object-choices.js
+++ javascript-tests/object-choices.js
@@ -1,24 +1,65 @@
-var simpleObject = {
-    sayName: function() {
-        console.log("Simple Object.");
+window.onload = () => {
+    
+    const simpleObjectAction = document.getElementById("simpleObjectAction");
+
+    simpleObjectAction.onclick = () => {
+        const simpleObjectDisplay = document.getElementById("simpleObjectDisplay");
+        simpleObjectDisplay.textContent = simpleObject.sayName();
     }
-}
 
-simpleObject.dynamicMethod = () => {
-    console.log("Dynamic Method.")
-}
+    const functionObjectAction = document.getElementById("functionObjectAction");
 
-function FunctionObject() {
-    FunctionObject.prototype.sayName = () => {
-        console.log("Function Object.");
+    functionObjectAction.onclick = () => {
+        const functionObjectDisplay = document.getElementById("functionObjectDisplay");
+        functionObjectDisplay.textContent = testObject.sayName();
     }
+
+    const customClassDisplay = document.getElementById("customClassDisplay");
+
+    customClassAction.onclick = () => {
+        const customClassDisplay = document.getElementById("customClassDisplay");
+        customClassDisplay.textContent = testClass.sayName();
+    }
+
+
+    var simpleObject = {
+        firstName: "M",
+        lastName: "A",
+        sayName: function() {
+            console.log(this.firstName + " " + this.lastName);
+            return this.firstName + " " + this.lastName;
+        }
+    }
+
+    simpleObject.dynamicMethod = () => {
+        console.log("Dynamic Method.")
+    }
+
+    function FunctionObject() {
+        FunctionObject.prototype.sayName = () => {
+            console.log("Function Object.");
+            return "Function Object";
+        }
+    }
+
+    class CustomClass {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        sayName() {
+            console.log(this.firstName + " " + this.lastName);
+            return this.firstName + " " + this.lastName;
+        }
+
+    }
+
+    simpleObject.sayName();
+    simpleObject.dynamicMethod();
+    const testObject = new FunctionObject();
+    testObject.sayName();
+    const testClass = new CustomClass("Custom", "Class");
+    testClass.sayName();
+
 }
-
-class CustomClass {
-
-}
-
-simpleObject.sayName();
-simpleObject.dynamicMethod();
-const testObject = new FunctionObject();
-testObject.sayName();
